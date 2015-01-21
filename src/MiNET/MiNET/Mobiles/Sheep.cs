974db514375f7e455979e8b4c425c3ff61ab8305
Nirad <miljours@gmail.com>
2015-01-20 23:24:09 -0500

    New added a basic base class for mobile and a Sheep class :)

---
+++ src/MiNET/MiNET/Mobiles/Sheep.cs
@@ -0,0 +1,21 @@
+﻿using MiNET.Worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiNET.Mobiles
+{
+    public class Sheep : Mobile
+    {
+        public override int Type { get { return 13; } }
+
+        public Sheep(Level level) : base(level)
+        {
+        }
+
+   
+    }
+
+}
